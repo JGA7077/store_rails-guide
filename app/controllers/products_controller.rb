c2e08c82118c383ac307aee20bb88769065c6114
João Gabriel <joaogabriel-al@hotmail.com>
2026-03-21 12:42:37 -0300

    adding rich text on product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,7 +39,7 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: %i[ name description ])
     end
 
     def set_product
